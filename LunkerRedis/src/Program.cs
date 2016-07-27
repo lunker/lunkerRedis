2b8228d1347cd8ea59f03fdb634895c84555c13a
이동규 [인턴] <dongqlee@ftt.inc>
2016-07-27 15:08:14 +0900

    scaffolding

---
+++ LunkerRedis/src/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LunkerRedis.src;
+
+
+
+namespace LunkerRedis
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+
+            RedisClient redisClient = new RedisClient();
+            if (redisClient.Connect())
+            {
+                Console.WriteLine("[Connect] success");
+            }
+            else
+            {
+                Console.WriteLine("[Connect] fail");
+            }
+
+            Console.ReadLine();// wait
+
+        }// end method
+
+
+    }
+}
+
+
+
+
+
+
